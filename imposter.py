5e271439e9f19491d7f56059ae86586cc9789919
Daniil Pomogaev <55309680+Redencon@users.noreply.github.com>
2023-05-01 16:08:22 +0300

    Imposter update

--- imposter.py
+++ imposter.py
@@ -1,24 +1,33 @@
 import telebot
 import json
+import yaml
 
-with open('secret2.json', 'r', encoding='utf-8') as f:
-    secret = json.load(f)
+for a in ('secret.json', 'secret2.json'):
+
+    with open(a, 'r', encoding='utf-8') as f:
+        secret = json.load(f)
+    with open('loc.yaml', 'r', encoding='utf-8') as f:
+        loc = yaml.safe_load(f)
 
 
-TOKEN = secret['TOKEN']
-ADMIN = secret['ADMIN']
-ARBITRARY_THRESHOLD = secret['ARBITRARY_THRESHOLD']
-CHAT = secret['CHAT']
+    TOKEN = secret['TOKEN']
+    ADMIN = secret['ADMIN']
+    ARBITRARY_THRESHOLD = secret['ARBITRARY_THRESHOLD']
+    CHAT = secret['CHAT']
+    STATUS_FILE = secret['STATUS_FILE']
 
-bot = telebot.TeleBot(TOKEN)
+    bot = telebot.TeleBot(TOKEN)
 
-# with open('goodnight.json', 'r') as file:
-#     gn = json.load(file)
+    with open(STATUS_FILE) as file:
+        users = json.load(file)
 
-with open('Nastya_Dicks.ogg', 'rb') as f:
-    mess = bot.send_voice(ADMIN, f)
-bot.send_voice(5050678382, mess.voice.file_id)
-# for user in [834476079, 264630131, 1311246531]:
-#     voice = bot.send_voice(user, mess.voice.file_id)
-#     print('sent!', end=' ')
-# print(mess.voice.file_id)
+    for user in users:
+        if users[user] is None: continue
+        try:
+            bot.send_message(
+                user,
+                loc['ru']['service']['beta_survey']
+            )
+            print('User {}: success'.format(user))
+        except:
+            print('User {}: failure'.format(user))
